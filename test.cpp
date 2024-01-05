c88cd373beb447a3bb3bb0aad7f3dce8d7bdccd6
unknown <maxfrenk@yahoo.com>
2024-01-05 11:36:05 -0500

    Added Random write test

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@
 #include <future>
 #include <string>
 #include <cassert>
+#include <random>
 
 using std::cout;
 using std::endl;
@@ -23,15 +24,21 @@ void write_from_to(int from, int to) {
     //return i;
 }
 
-void read_from_to(int from, int to) {
-    int i=from;
-    for(; i<to; ++i) {
-        std::string key = "key_" + std::to_string(i);
-        auto val = hashmap.get_value(key, -1);
-        //cout << key << ": " << val << endl;
-        //std::this_thread::sleep_for(0.1s);
-    }
-    //return 2*i;
+/// @brief Set to true while the reading thread is running, signalling to the writing 
+/// thread that not all values have yet been tested, and hence to keep writing
+std::atomic_bool keep_writing(true);
+
+void write_random(int from, int to) {
+    int i=0;
+    std::random_device rd;  // a seed source for the random number engine
+    std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> distrib(from, to);
+    do {
+        int rnd_int = distrib(gen);  // int in [from, to]
+        std::string key = "key_" + std::to_string(rnd_int);
+        hashmap.add_or_update(key, rnd_int);
+        ++i;
+    } while (keep_writing);
 }
 
 int main() {
@@ -46,17 +53,14 @@ int main() {
     hashmap.remove("2");
     assert(hashmap.get_size() == 0);
 
-    /////////////// Concurrent reads/writes /////////////////
     cout << "Hashmap created, spawning threads..." << endl;
     cout << "Hardware threads available: " << std::thread::hardware_concurrency << endl;
+
+    /////////////// Concurrent reads/writes - sequential /////////////////
+    std::cout << "Sequential reads/writes test" << std::endl;
     auto start = std::chrono::high_resolution_clock::now();
     auto fut_wr_1 = std::async(std::launch::async, write_from_to, 0, 1000);
     //auto fut_rd_1 = std::async(std::launch::async, read_from_to, 0, 500);
-    //auto fut_rd_2 = std::async(read_from_to, 334, 666);
-    //auto fut_rd_3 = std::async(read_from_to, 501, 750);
-    //auto fut_rd_4 = std::async(read_from_to, 601, 800);
-    ////////////////
-    // Perform more reads than writes
     for(int i=0; i<1000; ++i) {
         std::string key = "key_" + std::to_string(i);
         //std::this_thread::sleep_for(0.01s);
@@ -68,10 +72,31 @@ int main() {
         //cout << key << ": " << val << endl;
     }
     fut_wr_1.get();
-    //fut_rd_1.get();
-    //cout << 200 << " reads performed." << endl;
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> elapsed = end-start;
+    std::cout << elapsed.count() << " ms passed\n";
+    
+    /////////////// Concurrent reads/writes - random /////////////////
+    std::cout << "Random reads/writes test" << std::endl;
+    start = std::chrono::high_resolution_clock::now();
+    // write random keys into hashmap until all 1000 of them are read successfully
+    std::thread thrd([] {
+        write_random(0, 999); 
+    });
+    for(int i=0; i<=999; ++i) {
+        std::string key = "key_" + std::to_string(i);
+        int val;
+        do {
+            val = hashmap.get_value(key, -1);
+        } while(val == -1);
+        assert(val == i);
+        //std::cout << val << std::endl;
+    }
+    // if got here, all 1000 values were read successfully, can stop thrd
+    keep_writing = false;
+    thrd.join();
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end-start;
     std::cout << elapsed.count() << " ms passed\n";
     
     return 0;
